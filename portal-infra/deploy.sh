11c549a75cb962dcb1d8f4936a2b04ac970f7fd8
jinhyojin <hyojin@innogrid.com>
2020-09-08 14:05:06 +0900

    portal-min deployment
    portal-min deployment (1vm)

---
+++ portal-infra/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# VARIABLES
+BOSH_NAME="min-bosh"                                # bosh name (e.g. micro-bosh)
+IAAS="aws"                                      # IaaS (e.g. aws/azure/gcp/openstack/vsphere)
+COMMON_VARS_PATH="/home/ubuntu/workspace-min/paasta-5.0/deployment/common/common_vars.yml"            # common_vars.yml File Path (e.g. /home/ubuntu/paasta-5.0/common/common_vars.yml)
+
+# DEPLOY
+bosh -e ${BOSH_NAME} -n -d portal-infra deploy --no-redact portal-infra.yml \
+   -l ${COMMON_VARS_PATH} \
+   -l vars.yml
