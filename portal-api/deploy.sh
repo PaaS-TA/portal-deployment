c6b12e044bf183b5ede7a8f5284a157af4562632
lena <lena@irevit.co.kr>
2020-11-19 10:44:23 +0900

    Fix typo & update use-compiled-releases.yml

--- portal-api/deploy.sh
+++ portal-api/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # VARIABLES
-BOSH_NAME="micro-bosh"                                # bosh name (e.g. micro-bosh)
-IAAS="openstack"                                      # IaaS (e.g. aws/azure/gcp/openstack/vsphere)
-COMMON_VARS_PATH="<COMMON_VARS_FILE_PATH>"            # common_vars.yml File Path (e.g. /home/ubuntu/paasta-5.0/common/common_vars.yml)
+BOSH_NAME="<BOSH_NAME>"                                # bosh name (e.g. micro-bosh)
+IAAS="<IAAS_NAME>"                                     # IaaS (e.g. aws/azure/gcp/openstack/vsphere)
+COMMON_VARS_PATH="<COMMON_VARS_FILE_PATH>"             # common_vars.yml File Path (e.g. /home/ubuntu/paasta-5.0/common/common_vars.yml)
 
 # DEPLOY
 bosh -e ${BOSH_NAME} -n -d portal-api deploy --no-redact portal-api.yml \
--- portal-ui/deploy.sh
+++ portal-ui/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # VARIABLES
-BOSH_NAME="micro-bosh"                                          # bosh name (e.g. micro-bosh)
-IAAS="openstack"                                                # IaaS (e.g. aws/azure/gcp/openstack/vsphere)
+BOSH_NAME="<BOSH_NAME>"                                         # bosh name (e.g. micro-bosh)
+IAAS="<IAAS_NAME>"                                              # IaaS (e.g. aws/azure/gcp/openstack/vsphere)
 COMMON_VARS_PATH="<COMMON_VARS_FILE_PATH>"                      # common_vars.yml File Path (e.g. /home/ubuntu/workspace/paasta-5.0/common/common_vars.yml)
 
 # DEPLOY
