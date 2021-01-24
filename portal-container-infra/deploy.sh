cc9c01a7a0d86bf71cf22aae54e49ef72a08b844
okpc579 <ski579579@naver.com>
2021-01-24 13:19:52 +0900

    deploy.sh modify

--- portal-container-infra/deploy.sh
+++ portal-container-infra/deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # VARIABLES
-BOSH_NAME="<BOSH_NAME>"                                # bosh name (e.g. micro-bosh)
-IAAS="<IAAS_NAME>"                                     # IaaS (e.g. aws/azure/gcp/openstack/vsphere)
-COMMON_VARS_PATH="<COMMON_VARS_FILE_PATH>"             # common_vars.yml File Path (e.g. /home/ubuntu/paasta-5.0/common/common_vars.yml)
+COMMON_VARS_PATH="<COMMON_VARS_FILE_PATH>"             # common_vars.yml File Path (e.g. ../../common/common_vars.yml)
 
 # DEPLOY
-bosh -e ${BOSH_NAME} -n -d portal-container-infra deploy --no-redact portal-container-infra.yml \
+bosh -e ${BOSH_ENVIRONMENT} -n -d portal-container-infra deploy --no-redact portal-container-infra.yml \
    -l ${COMMON_VARS_PATH} \
    -l vars.yml
--- portal-ui/deploy.sh
+++ portal-ui/deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # VARIABLES
-BOSH_NAME="<BOSH_NAME>"                                         # bosh name (e.g. micro-bosh)
-IAAS="<IAAS_NAME>"                                              # IaaS (e.g. aws/azure/gcp/openstack/vsphere)
-COMMON_VARS_PATH="<COMMON_VARS_FILE_PATH>"                      # common_vars.yml File Path (e.g. /home/ubuntu/workspace/paasta-5.0/common/common_vars.yml)
+COMMON_VARS_PATH="<COMMON_VARS_FILE_PATH>"    # common_vars.yml File Path (e.g. ../../common/common_vars.yml)
 
 # DEPLOY
-bosh -e ${BOSH_NAME} -n -d portal-ui deploy portal-ui.yml \
-    -o operations/${IAAS}-network.yml \
+bosh -e ${BOSH_ENVIRONMENT} -n -d portal-ui deploy portal-ui.yml \
+    -o operations/${CURRENT_IAAS}-network.yml \
     -l ${COMMON_VARS_PATH} \
     -l vars.yml
